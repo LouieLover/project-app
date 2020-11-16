ee5737818e7a78d305df8d995ef9f072435c6d68
Larry X <papadocarock@gmail.com>
2020-11-16 02:03:04 -0500

    working router

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CreateTeam from "./components/createTeam";
 import EditTeam from "./components/editTeam";
 import TeamList from "./components/teamList";
-
+import Nav from "./components/nav";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 // import user from "./components/auth/AuthOptions";
@@ -65,52 +65,7 @@ function App() {
 
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="login">
-          SportsMeet
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="/login">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/register">
-                Register
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/teams">
-                Team List
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/user">
-                New Team
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/edit/:id">
-                Edit Team
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
+      <Nav />
       <br />
       <UserContext.Provider value={{ userData, setUserData }}>
         <Route path="/login" component={Login} />
